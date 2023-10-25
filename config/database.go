533f8ebfd59837330117e413e59ce28337a63e5c
haris ardian <harisfirman387@gmail.com>
2023-10-25 19:08:55 +0700

    refactor

--- config/database.go
+++ config/database.go
@@ -2,30 +2,27 @@ package config
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 
 	_ "github.com/go-sql-driver/mysql" // Import driver MySQL
 )
 
-func ConnectDb() {
+func ConnectDb() (*sql.DB, error) {
 	// Konfigurasi koneksi database
 	dsn := "root:@tcp(localhost:3306)/aktif_hutan"
 
 	// Membuat koneksi database
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
-		fmt.Println("Gagal menghubungkan ke database:", err)
-		return
+		return nil, err
 	}
-	defer db.Close()
 
 	// Menguji koneksi
 	err = db.Ping()
 	if err != nil {
-		fmt.Println("Gagal menghubungkan ke database:", err)
-		return
+		return nil, err
 	}
 
 	log.Println("Sukses terhubung ke database!")
+	return db, nil
 }
