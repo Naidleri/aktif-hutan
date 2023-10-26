f46259bd2f20106d6a2cfb045b28734181e37e49
haris ardian <harisfirman387@gmail.com>
2023-10-26 22:40:16 +0700

    feat : Add kegiatan fetch

---
+++ controller/homecontroller/home.go
@@ -0,0 +1,37 @@
+package homecontroller
+
+import (
+	"aktif_hutan/config"
+	"aktif_hutan/model/homemodel"
+	"html/template"
+	"net/http"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		db := config.ConnectDb()
+		kegiatanModel := homemodel.NewKegiatanModel(db)
+
+		// Mengambil data kegiatan dari model
+		kegiatans, err := kegiatanModel.GetKegiatanData()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Menetapkan data Kegiatans ke tampilan
+		data := struct {
+			Kegiatans []homemodel.Kegiatan // Sesuaikan dengan struktur model Anda
+		}{
+			Kegiatans: kegiatans,
+		}
+
+		// Eksekusi template dengan data
+		temp, err := template.ParseFiles("view/home/homepage.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		temp.Execute(w, data)
+	}
+}
