ff58f6466e6097ae5862d982227135cdfc2e17b6
haris ardian <harisfirman387@gmail.com>
2023-10-26 08:52:26 +0700

    update

--- config/database.go
+++ config/database.go
@@ -7,22 +7,26 @@ import (
 	_ "github.com/go-sql-driver/mysql" // Import driver MySQL
 )
 
-func ConnectDb() (*sql.DB, error) {
+var db *sql.DB
+
+func ConnectDb() *sql.DB {
 	// Konfigurasi koneksi database
 	dsn := "root:@tcp(localhost:3306)/aktif_hutan"
 
 	// Membuat koneksi database
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
-		return nil, err
+		log.Println("Gagal menghubungkan ke database:", err)
+		return nil
 	}
 
 	// Menguji koneksi
 	err = db.Ping()
 	if err != nil {
-		return nil, err
+		log.Println("Gagal menghubungkan ke database:", err)
+		return nil
 	}
 
 	log.Println("Sukses terhubung ke database!")
-	return db, nil
+	return db
 }
